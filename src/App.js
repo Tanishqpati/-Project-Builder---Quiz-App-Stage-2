bd798ac42c4356dc87a271d816cc27d432171bbd
Tanishqpati <113436940+Tanishqpati@users.noreply.github.com>
2023-01-11 00:15:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import HomeComponent from './components/HomeComponent';
+import Quizcomponent from './components/Quizcomponent';
+import Resultcomponent from './components/Resultcomponent';
+
+function App() {
+  return (
+    <div className="App">
+       
+     <HomeComponent/>
+     <Quizcomponent/>
+     <Resultcomponent/>
+    </div>
+  );
+}
+
+export default App;
